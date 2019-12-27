81a495d2877fdba70e8f3c2545eab185c84e6dc8
RamonFern <ramonfernandes9@gmail.com>
2019-12-26 22:55:35 -0300

    Projeto criado

---
+++ src/main/java/com/ramon/projeto_transito/ProjetoTransitoApplication.java
@@ -0,0 +1,13 @@
+package com.ramon.projeto_transito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTransitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTransitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramon/projeto_transito/ProjetoTransitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramon.projeto_transito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTransitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
