32c85e0a3ba22e83087b35beb4a647639850b82b
RamonFern <ramonfernandes9@gmail.com>
2020-01-03 22:43:12 -0300

    Criando operação de instaciação

--- src/main/java/com/ramon/projeto_transito/ProjetoTransitoApplication.java
+++ src/main/java/com/ramon/projeto_transito/ProjetoTransitoApplication.java
@@ -1,13 +1,36 @@
 package com.ramon.projeto_transito;
 
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ramon.projeto_transito.domain.Categoria;
+import com.ramon.projeto_transito.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ProjetoTransitoApplication {
+public class ProjetoTransitoApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoTransitoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Veiculo de Passageiros");
+		Categoria cat2 = new Categoria(null, "Veículo de Carga");
+		
+		categoriaRepository.save(cat1);
+		categoriaRepository.save(cat2);
+		
+	    // categoriaRepository.save(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
